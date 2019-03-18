490c8687ccec57b5a444542fd47a365d2200dc56
Martin Giger <martin@humanoids.be>
2019-03-18 19:53:30 +0100

    Initial adapter

---
+++ README.md
@@ -0,0 +1,11 @@
+# Max Hauri maxSMART 2.0 Adapter
+
+Integrates the Max Hauri maxSMART 2.0 clip-clap Switch WiFi with the Mozilla IoT gateway.
+
+May also support other devices made by Revogi.
+
+## Pairing
+
+The adapter doesn't support any of the pairing protocols nor the cloud API used by the app. This means you have to manually set up your smart plug using the official app. After you have it set up, you can then go into the app and get the serial number ("S/N") in the device details. To get the IP, you'll likely have to log in to your router. Enter those two things in the configuration details of the adapter.
+
+After that, the switch should show up in the pairing screen. Note that you have to manually set the name and the initial power state may be wrong.
---
+++ index.js
@@ -0,0 +1,130 @@
+// Copyright (c) 2019 Martin Giger
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+const {Adapter, Device, Property} = require('gateway-addon');
+const maxsmart = require("mh-maxsmart2");
+
+class ReadonlyProperty extends Property {
+    constructor(device, name, description) {
+        description.readOnly = true;
+        super(device, name, description);
+    }
+
+    async setValue(value) {
+        return Promise.reject("Read-only property");
+    }
+
+    setReadonly(value) {
+        if(value !== this.value) {
+            this.setCachedValue(value);
+            this.device.notifyPropertyChanged(this);
+            if(this.value != this.prevSetValue) {
+                console.log('setValue for property', this.name, 'to', this.value, 'for', this.device.id);
+                this.prevSetValue = this.value;
+            }
+        }
+    }
+}
+
+
+class Plug extends Device {
+    constructor(adapter, desc) {
+        super(adapter, desc.sn);
+        this.name = desc.sn;
+        this.udpDevice = desc;
+        this.description = "Max Hauri maxSMART 2.0 clip-clap Switch WiFi";
+        this['@type'] = [ 'SmartPlug' ];
+
+        this.properties.set('on', new Property(this, 'on', {
+            type: 'boolean',
+            '@type': 'OnOffProperty',
+            label: 'On'
+        }));
+        this.properties.set('watt', new ReadonlyProperty(this, 'watt', {
+            type: 'number',
+            '@type': 'InstantaneousPowerProperty',
+            unit: 'watt',
+            label: 'Power'
+        }));
+        this.properties.set('amp', new ReadonlyProperty(this, 'amp', {
+            type: 'number',
+            '@type': 'CurrentProperty',
+            unit: 'ampere',
+            label: 'Current'
+        }));
+
+        this.update();
+    }
+
+    udpSend(command, data = {}) {
+        return maxsmart.send(this.udpDevice.sn, this.udpDevice.ip, command, data);
+    }
+
+    async update() {
+        try {
+            const res = await this.udpSend(maxsmart.CMD.GET_WATT);
+            this.findProperty('watt').setReadonly(res.watt);
+            this.findProperty('amp').setReadonly(res.amp);
+        }
+        catch(e) {
+            console.error(e);
+        }
+    }
+
+    async notifyPropertyChanged(property) {
+        if(property.name === 'on') {
+            await this.udpSend(maxsmart.CMD.CONTROL, maxsmart.makeControlPacket(property.value));
+        }
+        super.notifyPropertyChanged(property);
+    }
+}
+
+class MaxSmartAdapter extends Adapter {
+    constructor(addonManager, name, config) {
+        super(addonManager, 'MaxSmart2Adapter', name);
+        addonManager.addAdapter(this);
+
+        for(const device of config.devices) {
+            this.addDevice(device);
+        }
+    }
+
+    addDevice(desc) {
+        const device = new Plug(this, desc);
+        if(!this.interval) {
+            this.interval = setInterval(() => this.updateDevices(), 10000);
+        }
+        this.handleDeviceAdded(device);
+    }
+
+    handleDeviceRemoved(device) {
+        device.unload();
+        super.handleDeviceRemoved(device);
+        if(this.interval && !Object.keys(this.devices).length) {
+            clearInterval(this.interval);
+            delete this.interval;
+        }
+    }
+
+    unload() {
+        if(this.interval) {
+            clearInterval(this.interval);
+            delete this.interval;
+        }
+        return super.unload();
+    }
+
+    updateDevices() {
+        for(const device in this.devices) {
+            if(this.devices.hasOwnProperty(device)) {
+                this.devices[device].update();
+            }
+        }
+    }
+}
+
+module.exports = (addonManager, manifest, reportError) => {
+    new MaxSmartAdapter(addonManager, manifest.name, manifest.moziot.config);
+};
